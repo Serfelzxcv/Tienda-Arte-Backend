e32fc200c4a8d6be7234160922c79fdd0cc503f9
Serfelzxcv <desarrollador.05.9780@gmail.com>
2025-03-26 11:43:27 -0500

    back register terminado

--- tienda/serializers.py
+++ tienda/serializers.py
@@ -8,18 +8,18 @@ class RegistroSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
-        # Crear carrito automáticamente
         Carrito.objects.create(usuario=user)
         return user
-
 
 # Producto
 class ProductoSerializer(serializers.ModelSerializer):
